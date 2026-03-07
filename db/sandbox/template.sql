e1bdecdc086b86d635a9121d06a4b11aaf328b21
Cedric McKeever <cedricmckeever@gmail.com>
2026-03-07 13:36:21 +0530

    delete sandbox.sql and gitignore it

---
+++ db/sandbox/template.sql
@@ -0,0 +1,9 @@
+-- Use the `template.sql` file to create `sandbox.sql` to test SQL queries/migrations.
+-- You can then use `run.sh` file to conveniently run this file!
+-- By default, it uses the migrator role, so be careful future me :)
+
+BEGIN;
+
+-- Your SQL statements here
+
+ROLLBACK;
